90fccd4459ec80c3c0a365a905f054eb5d2b31e6
Slav Pisarov <slav.pisarov@gmail.com>
2023-11-15 18:41:43 +0200

    Filter comments by post

--- client/src/services/commentService.js
+++ client/src/services/commentService.js
@@ -3,10 +3,10 @@ import * as request from '../lib/request'
 const baseUrl = 'http://localhost:3030/jsonstore/comments'
 
 
-export const getAll = async () =>{
+export const getAll = async (gameId) =>{
     const result = await request.get(baseUrl);
 
-    return Object.values(result);
+    return Object.values(result).filter(comment => comment.gameId === gameId);
 }
 
 
