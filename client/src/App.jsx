04a364310ef036bfe56106d3f5f89a5c90d434e0
Slav Pisarov <slav.pisarov@gmail.com>
2023-11-15 13:09:57 +0200

    Add catalog page
    Set navigation

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,8 @@
+import {Routes, Route} from 'react-router-dom'
+
 import Header from "./components/Header/Header"
 import Home from "./components/Home/Home"
+import GameList from './components/GameList/GameList'
 
 
 function App() {
@@ -7,7 +10,11 @@ function App() {
   return (
     <div id="box">
         <Header/>
-        <Home />
+
+        <Routes>
+            <Route path='/' element={<Home/>}/>
+            <Route path='/games' element={<GameList/>}/>
+        </Routes>
     </div>
   )
 }
