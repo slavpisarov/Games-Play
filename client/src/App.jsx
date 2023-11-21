cb8a1eb878664df661f62341dcd2e0137e41b0bd
Slav Pisarov <slav.pisarov@gmail.com>
2023-11-21 18:18:31 +0200

    Change register form to controlled

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react'
-import {Routes, Route, useNavigate} from 'react-router-dom'
+import { Routes, Route, useNavigate } from 'react-router-dom'
 
 import * as authService from './services/authService'
 import AuthContext from './contexts/AuthContext'
@@ -15,33 +15,42 @@ import GameDetails from './components/GameDetails/GameDetails'
 
 function App() {
   const navigate = useNavigate()
-  const [auth,setAuth] = useState({})
+  const [auth, setAuth] = useState({})
 
-  const loginSubmitHandler = async (values) =>{
-    const result = await authService.login(values.email,values.password)
-    console.log(values);
+  const loginSubmitHandler = async (values) => {
+    const result = await authService.login(values.email, values.password)
     setAuth(result);
     navigate('/')
-  }  
-  const values ={
-    loginSubmitHandler,
-    email:auth.email,
-    isAuthenticated:!!auth.accessToken,
   }
+
+  const registerSubmitHandler = async (values) => {
+    console.log(values);
+    // const result = await authService.login(values.email,values.password)
+    // setAuth(result);
+    // navigate('/')
+  }
+
+  const values = {
+    registerSubmitHandler,
+    loginSubmitHandler,
+    email: auth.email,
+    isAuthenticated: !!auth.accessToken,
+  }
+
   return (
     <AuthContext.Provider value={values}>
-    <div id="box">
-        <Header/>
+      <div id="box">
+        <Header />
 
         <Routes>
-            <Route path='/' element={<Home/>}/>
-            <Route path='/games' element={<GameList/>}/>
-            <Route path='/games/:gameId' element={<GameDetails/>}/>
-            <Route path='/create' element={<GameCreate/>}/>
-            <Route path='/login' element={<Login />}/>
-            <Route path='/register' element={<Register/>}/>
+          <Route path='/' element={<Home />} />
+          <Route path='/games' element={<GameList />} />
+          <Route path='/games/:gameId' element={<GameDetails />} />
+          <Route path='/create' element={<GameCreate />} />
+          <Route path='/login' element={<Login />} />
+          <Route path='/register' element={<Register />} />
         </Routes>
-    </div>
+      </div>
     </AuthContext.Provider>
   )
 }
--- client/src/components/Register/Register.jsx
+++ client/src/components/Register/Register.jsx
@@ -1,19 +1,50 @@
+import { useContext } from "react"
+import AuthContext from "../../contexts/AuthContext"
+import useForm from "../../hooks/useForm"
+
 export default function Register() {
+
+    const {registerSubmitHandler} =useContext(AuthContext)
+    const {values, onChange, onSubmit} = useForm(registerSubmitHandler,{
+        'email':'',
+        'password':'',
+        'confirm-password':'',
+    })
+ 
     return (
         <section id="register-page" className="content auth">
-            <form id="register">
+            <form id="register" onSubmit={onSubmit}>
                 <div className="container">
                     <div className="brand-logo"></div>
                     <h1>Register</h1>
 
                     <label htmlFor="email">Email:</label>
-                    <input type="email" id="email" name="email" placeholder="maria@email.com" />
+                    <input
+                    type="email"
+                    id="email"
+                    name="email"
+                    placeholder="maria@email.com"
+                    onChange={onChange}
+                    value={values.email}
+                    />
 
-                    <label htmlFor="pass">Password:</label>
-                    <input type="password" name="password" id="register-password" />
+                   <label htmlFor="pass">Password:</label>
+                    <input
+                    type="password"
+                    name="password"
+                    id="register-password"
+                    onChange={onChange}
+                    value={values.password}
+                    />
 
-                    <label htmlFor="con-pass">Confirm Password:</label>
-                    <input type="password" name="confirm-password" id="confirm-password" />
+                   <label htmlFor="con-pass">Confirm Password:</label>
+                    <input
+                    type="password"
+                    name="confirm-password"
+                    id="confirm-password"
+                    onChange={onChange}
+                    value={values['confirm-password']}
+                    />
 
                     <input className="btn submit" type="submit" value="Register" />
 
